f1a60fb456b5af5b615fbfa0ba89bb98c71b10a8
SY <pkid678@gmail.com>
2025-06-10 01:14:55 +0800

    feat: add components to page
    feat: add components to page

--- src/app/components/game8.js
+++ src/app/components/game8.js
@@ -3,6 +3,11 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../AuthContext";
 
+import Order from "./game8/order.js";
+import Timer from "./game8/timer.js";
+import Coin from "./game8/coin.js";
+import Ice from "./game8/ice.js";
+
 export default function Game8Canvas() {
   const [success, setSuccess] = useState(false);
   const [showOverlay, setShowOverlay] = useState(false);
@@ -11,7 +16,7 @@ export default function Game8Canvas() {
   useEffect(() => {
     const hasCleared = localStorage.getItem("game2Success") === "true";
     if (hasCleared) {
-      setSuccess(false); 
+      setSuccess(false);
       setShowOverlay(false);
     }
   }, []);
@@ -44,7 +49,7 @@ export default function Game8Canvas() {
   return (
     <>
       {/* 這裡放你的遊戲元件或互動內容，預設只留一個模擬過關按鈕 */}
-      <button
+      {/* <button
         onClick={handleSuccess}
         style={{
           padding: "12px 32px",
@@ -57,7 +62,107 @@ export default function Game8Canvas() {
         }}
       >
         模擬過關8（請改成你自己的過關條件）
-      </button>
+      </button> */}
+
+      {/* order list*/}
+      <div
+        style={{
+          background: "#C5AC6B",
+          borderRadius: 12,
+          height: "100%",
+          width: "100%",
+          maxWidth: "200px",
+          margin: "10px",
+          padding: "15px",
+          boxSizing: "border-box",
+        }}>
+
+        <Order orders={[
+          { name: "green" },
+          { name: "cherry" },
+          { name: "watermelon" }
+        ]} />
+
+      </div>
+
+      {/* ice*/}
+      <div
+        style={{
+          background: "#505166",
+          borderRadius: 12,
+          height: "100%",
+          width: "100%",
+          minWidth: "250px",
+          maxWidth: "500px",
+          margin: "10px",
+        }}>
+
+        <Ice components={[
+          { name: "ice" },
+          { name: "ice" },
+          { name: "ice" }
+        ]} />
+      </div>
+
+      {/* right-bottom*/}
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          maxWidth: "300px",
+          margin: "10px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+        }}>
+
+        {/* time*/}
+        <div
+          style={{
+            background: "white",
+            borderRadius: 12,
+            flex: 1,
+          }}>
+
+          <Timer duration={100} onEnd={() => {
+            // 時間結束時的處理
+          }}></Timer>
+
+        </div>
+
+        {/* serve*/}
+        <button
+          /*onClick={}*/
+          style={{
+            background: "#E36B5B",
+            borderRadius: 12,
+            flex: 1,
+            cursor: "pointer",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "clamp(1rem, 4vw, 2rem)",
+            fontWeight: "bold",
+            letterSpacing: 10,
+            border: "none",
+            color: "white"
+          }}
+        >
+          出餐
+        </button>
+
+        {/* coin */}
+        <div
+          style={{
+            background: "white",
+            borderRadius: 12,
+            flex: 1
+          }}>
+
+          <Coin coins={100} />
+        </div>
+
+      </div>
 
       {/* 成功過關後彈窗 */}
       {showOverlay && (
--- src/app/game8/page.js
+++ src/app/game8/page.js
@@ -7,12 +7,17 @@ export default function App() {
       style={{
         height: 'calc(100vh - 160px)',
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: 120,
         marginBottom: 140,
         border: '3px solid #C5AC6B',
+        paddingTop: 20,
+        paddingBottom: 20,
+        paddingLeft: 10,
+        paddingRight: 10,
+        boxSizing: 'border-box',
         boxShadow: '0 2px 8px rgba(0,0,0,0.03)',
         position: 'relative',
       }}
